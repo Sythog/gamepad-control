7f2f62fa6947ba865324049cb0c6e32245f9ef65
Sythog <megaghosty@gmail.com>
2015-01-08 10:14:11 +0100

    Added FunctionalInterface anotation

--- src/main/java/com/ghosty/desktop/gamepad/handler/ControllerEventHandler.java
+++ src/main/java/com/ghosty/desktop/gamepad/handler/ControllerEventHandler.java
@@ -2,6 +2,9 @@ package com.ghosty.desktop.gamepad.handler;
 
 import net.java.games.input.Component;
 
+import java.lang.FunctionalInterface;
+
+@FunctionalInterface
 public interface ControllerEventHandler {
 
     void handle(Component event);
